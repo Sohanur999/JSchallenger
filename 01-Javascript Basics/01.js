81229f93b99dc4f39b53222cbafe3a46c82739df
Sohanur Rahman <sohanur999rahman@gmail.com>
2022-07-25 22:26:30 +0600

    first commit

---
+++ 01-Javascript Basics/01.js
@@ -0,0 +1,10 @@
+//Sum of two number
+
+const a = parseInt(prompt("Enter first number"));
+const b = parseInt(prompt("Enter second number"));
+
+function myFunction(a,b) {
+    return (a+b);
+}
+const c = myFunction(a,b);
+console.log(c);
